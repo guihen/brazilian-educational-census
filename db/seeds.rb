3974db52dda92a5f1b0a750627482bbab8ecb524
Guilherme Rojas <guilhermehrojas@gmail.com>
2022-11-22 18:54:13 -0300

    Adds feedbacks for seeds script
    As it takes a while to run, it seems nice having feedbacks about the amount of records saved and the elapsed time.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,37 @@
 require Rails.root.join('lib', 'census_data', 'school_dto')
 
-file_2021 = File.open(Rails.application.path_to_census_2021)
+# shows elapsed time prettier
+include ActionView::Helpers::DateHelper
+
+# avoids printing when the tests are running
+class SeedPrinter
+  def self.print(string)
+    puts string unless Rails.env.test?
+  end
+end
+
+
+file_2021 = File.open(Rails.application.path_to_census_2021, :encoding => 'iso-8859-1')
 
 # ignore the header
 file_2021.readline
 
-file_2021.each do |line|
-  dto = SchoolDto.new(line)
-  record = School.new
-  record.code = dto.code
-  record.name = dto.name
-  record.region = dto.region
-  record.state = dto.state
-  record.county = dto.county
-  record.zipcode = dto.zipcode
-  record.save
+SeedPrinter.print 'Importing file and saving it. It may take a while...'
+start_time = Time.now
+
+School.transaction do
+  file_2021.each do |line|
+    dto = SchoolDto.new(line)
+    record = School.new
+    record.code = dto.code
+    record.name = dto.name
+    record.region = dto.region
+    record.state = dto.state
+    record.county = dto.county
+    record.zipcode = dto.zipcode
+    record.save
+  end
 end
+
+SeedPrinter.print "Importante successfully. Imported #{School.count} records"
+SeedPrinter.print "Elapsed time: #{ActionView::Helpers::DateHelper::distance_of_time_in_words(start_time, Time.now)}"
