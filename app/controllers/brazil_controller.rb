6e7b8b70e1d6cc0c72a41b667c143a284ed9e639
Guilherme Rojas <guilhermehrojas@gmail.com>
2022-11-24 15:55:34 -0300

    Adds total schools grouped by region.
    PENDING: Once I finish the statistics that I want to show, I'll improve the page layout.

--- app/controllers/brazil_controller.rb
+++ app/controllers/brazil_controller.rb
@@ -1,5 +1,6 @@
 class BrazilController < ApplicationController
   def show
     @total_schools = School.all.count
+    @total_schools_by_region = School.group(:region).count
   end
 end
--- test/system/brazil_test.rb
+++ test/system/brazil_test.rb
@@ -16,4 +16,16 @@ class BrazilTest < ApplicationSystemTestCase
 
     assert_text total_schools
   end
+
+  test "should have the total schools grouped by region" do
+    total_schools_by_region = School.group(:region).count
+
+    visit brazil_show_url
+
+    assert_text "Região Norte: #{total_schools_by_region["Norte"]}"
+    assert_text "Região Nordeste: #{total_schools_by_region["Nordeste"]}"
+    assert_text "Região Centro-Oeste: #{total_schools_by_region["Centro-Oeste"]}"
+    assert_text "Região Sudeste: #{total_schools_by_region["Sudeste"]}"
+    assert_text "Região Sul: #{total_schools_by_region["Sul"]}"
+  end
 end
