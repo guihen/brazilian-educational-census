aeb19338bc605012080290a54b1ada47d09c8043
Guilherme Rojas <guilhermehrojas@gmail.com>
2022-11-26 12:40:39 -0300

    Adds the accumulated racial diversity for the entire country.
    MY-BAD: it seems that I had not added the view before, so the previous commits may no work properly!
    TODO: Adds the accumulated racial diversity for each region.

--- app/controllers/brazil_controller.rb
+++ app/controllers/brazil_controller.rb
@@ -2,5 +2,15 @@ class BrazilController < ApplicationController
   def show
     @total_schools = School.all.count
     @total_schools_by_region = School.group(:region).count
+    @racial_diversity_totals = School.pluck(:racial_diversity).reduce(Hash.new(0)) do |accumulated, racial_diversity|
+      {
+        "nao_declarado" => accumulated["nao_declarado"] + racial_diversity["nao_declarado"],
+        "branca" => accumulated["branca"] + racial_diversity["branca"],
+        "parda" => accumulated["parda"] + racial_diversity["parda"],
+        "preta" => accumulated["preta"] + racial_diversity["preta"],
+        "amarela" => accumulated["amarela"] + racial_diversity["amarela"],
+        "indigena" => accumulated["indigena"] + racial_diversity["indigena"]
+      }
+    end
   end
 end
--- test/system/brazil_test.rb
+++ test/system/brazil_test.rb
@@ -28,4 +28,26 @@ class BrazilTest < ApplicationSystemTestCase
     assert_text "Região Sudeste: #{total_schools_by_region["Sudeste"]}"
     assert_text "Região Sul: #{total_schools_by_region["Sul"]}"
   end
+
+  test "should have the racial diversity accumulated for the entire country" do
+    racial_diversity_totals = School.pluck(:racial_diversity).reduce(Hash.new(0)) do |accumulated, racial_diversity|
+      {
+        "nao_declarado" => accumulated["nao_declarado"] + racial_diversity["nao_declarado"],
+        "branca" => accumulated["branca"] + racial_diversity["branca"],
+        "parda" => accumulated["parda"] + racial_diversity["parda"],
+        "preta" => accumulated["preta"] + racial_diversity["preta"],
+        "amarela" => accumulated["amarela"] + racial_diversity["amarela"],
+        "indigena" => accumulated["indigena"] + racial_diversity["indigena"]
+      }
+    end
+
+    visit brazil_show_url
+
+    assert_text "Total de Não Declarados: #{racial_diversity_totals["nao_declarado"]}"
+    assert_text "Total de Brancas: #{racial_diversity_totals["branca"]}"
+    assert_text "Total de Pardas: #{racial_diversity_totals["parda"]}"
+    assert_text "Total de Pretas: #{racial_diversity_totals["preta"]}"
+    assert_text "Total de Amarelas: #{racial_diversity_totals["amarela"]}"
+    assert_text "Total de Indígenas: #{racial_diversity_totals["indigena"]}"
+  end
 end
