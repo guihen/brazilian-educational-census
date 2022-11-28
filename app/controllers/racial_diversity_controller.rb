dec2212927397499bebfc7e6a393705837d9ea52
Guilherme Rojas <guilhermehrojas@gmail.com>
2022-11-27 22:55:15 -0300

    Adds racial diversity for all regions of the country.

--- app/controllers/racial_diversity_controller.rb
+++ app/controllers/racial_diversity_controller.rb
@@ -1,7 +1,12 @@
 class RacialDiversityController < ApplicationController
   def index
-    @racial_diversity = sum_racial_diversity(School.pluck(:racial_diversity))
-    @racial_diversity_region_norte = sum_racial_diversity(School.where(region: "Norte").pluck(:racial_diversity))
+    @racial_diversity = Hash.new
+    @racial_diversity["Total"] = sum_racial_diversity(School.pluck(:racial_diversity))
+    @racial_diversity["Norte"] = sum_racial_diversity(School.where(region: "Norte").pluck(:racial_diversity))
+    @racial_diversity["Nordeste"] = sum_racial_diversity(School.where(region: "Nordeste").pluck(:racial_diversity))
+    @racial_diversity["Centro-Oeste"] = sum_racial_diversity(School.where(region: "Centro-Oeste").pluck(:racial_diversity))
+    @racial_diversity["Sudeste"] = sum_racial_diversity(School.where(region: "Sudeste").pluck(:racial_diversity))
+    @racial_diversity["Sul"] = sum_racial_diversity(School.where(region: "Sul").pluck(:racial_diversity))
   end
 
   private
--- test/system/racial_diversity_tests.rb
+++ test/system/racial_diversity_tests.rb
@@ -43,42 +43,120 @@ class RacialDiversityTest < ApplicationSystemTestCase
     end
   end
 
-  test "should have the racial diversity for the region Norte" do
-    racial_diversity_by_region = School.where(region: "Norte").pluck(:racial_diversity).reduce(Hash.new(0)) do |memo, racial_diversity|
-      {
-        "nao_declarado" => memo["nao_declarado"] + racial_diversity["nao_declarado"],
-        "branca" => memo["branca"] + racial_diversity["branca"],
-        "parda" => memo["parda"] + racial_diversity["parda"],
-        "preta" => memo["preta"] + racial_diversity["preta"],
-        "amarela" => memo["amarela"] + racial_diversity["amarela"],
-        "indigena" => memo["indigena"] + racial_diversity["indigena"]
-      }
+  test "should have the racial diversity grouped by region" do
+    structure = {
+      "Norte" => [],
+      "Nordeste" => [],
+      "Centro-Oeste" => [],
+      "Sudeste" => [],
+      "Sul" => []
+    }
+    racial_diversity_grouped_by_region = School.pluck(:region, :racial_diversity).reduce(structure) do |memo, record|
+      memo[record.first] << record.last
+      memo
+    end
+
+    racial_diversity_grouped_by_region.each do |region, racial_diversities|
+      racial_diversity_grouped_by_region[region] = SumRacialDiversity.new(racial_diversities).call
     end
 
     visit racial_diversity_index_url
 
+    within(id: "nao-declarado-regiao-nordeste") do
+      assert_text number_with_delimiter(racial_diversity_grouped_by_region["Nordeste"]["nao_declarado"])
+    end
+    within(id: "branca-regiao-nordeste") do
+      assert_text number_with_delimiter(racial_diversity_grouped_by_region["Nordeste"]["branca"])
+    end
+    within(id: "parda-regiao-nordeste") do
+      assert_text number_with_delimiter(racial_diversity_grouped_by_region["Nordeste"]["parda"])
+    end
+    within(id: "preta-regiao-nordeste") do
+      assert_text number_with_delimiter(racial_diversity_grouped_by_region["Nordeste"]["preta"])
+    end
+    within(id: "amarela-regiao-nordeste") do
+      assert_text number_with_delimiter(racial_diversity_grouped_by_region["Nordeste"]["amarela"])
+    end
+    within(id: "indigena-regiao-nordeste") do
+      assert_text number_with_delimiter(racial_diversity_grouped_by_region["Nordeste"]["indigena"])
+    end
+
+    within(id: "nao-declarado-regiao-sudeste") do
+      assert_text number_with_delimiter(racial_diversity_grouped_by_region["Sudeste"]["nao_declarado"])
+    end
+    within(id: "branca-regiao-sudeste") do
+      assert_text number_with_delimiter(racial_diversity_grouped_by_region["Sudeste"]["branca"])
+    end
+    within(id: "parda-regiao-sudeste") do
+      assert_text number_with_delimiter(racial_diversity_grouped_by_region["Sudeste"]["parda"])
+    end
+    within(id: "preta-regiao-sudeste") do
+      assert_text number_with_delimiter(racial_diversity_grouped_by_region["Sudeste"]["preta"])
+    end
+    within(id: "amarela-regiao-sudeste") do
+      assert_text number_with_delimiter(racial_diversity_grouped_by_region["Sudeste"]["amarela"])
+    end
+    within(id: "indigena-regiao-sudeste") do
+      assert_text number_with_delimiter(racial_diversity_grouped_by_region["Sudeste"]["indigena"])
+    end
+
+    within(id: "nao-declarado-regiao-sul") do
+      assert_text number_with_delimiter(racial_diversity_grouped_by_region["Sul"]["nao_declarado"])
+    end
+    within(id: "branca-regiao-sul") do
+      assert_text number_with_delimiter(racial_diversity_grouped_by_region["Sul"]["branca"])
+    end
+    within(id: "parda-regiao-sul") do
+      assert_text number_with_delimiter(racial_diversity_grouped_by_region["Sul"]["parda"])
+    end
+    within(id: "preta-regiao-sul") do
+      assert_text number_with_delimiter(racial_diversity_grouped_by_region["Sul"]["preta"])
+    end
+    within(id: "amarela-regiao-sul") do
+      assert_text number_with_delimiter(racial_diversity_grouped_by_region["Sul"]["amarela"])
+    end
+    within(id: "indigena-regiao-sul") do
+      assert_text number_with_delimiter(racial_diversity_grouped_by_region["Sul"]["indigena"])
+    end
+
+    within(id: "nao-declarado-regiao-centro-oeste") do
+      assert_text number_with_delimiter(racial_diversity_grouped_by_region["Centro-Oeste"]["nao_declarado"])
+    end
+    within(id: "branca-regiao-centro-oeste") do
+      assert_text number_with_delimiter(racial_diversity_grouped_by_region["Centro-Oeste"]["branca"])
+    end
+    within(id: "parda-regiao-centro-oeste") do
+      assert_text number_with_delimiter(racial_diversity_grouped_by_region["Centro-Oeste"]["parda"])
+    end
+    within(id: "preta-regiao-centro-oeste") do
+      assert_text number_with_delimiter(racial_diversity_grouped_by_region["Centro-Oeste"]["preta"])
+    end
+    within(id: "amarela-regiao-centro-oeste") do
+      assert_text number_with_delimiter(racial_diversity_grouped_by_region["Centro-Oeste"]["amarela"])
+    end
+    within(id: "indigena-regiao-centro-oeste") do
+      assert_text number_with_delimiter(racial_diversity_grouped_by_region["Centro-Oeste"]["indigena"])
+    end
+
     within(id: "nao-declarado-regiao-norte") do
-        assert_text number_with_delimiter(racial_diversity_by_region["nao_declarado"])
+        assert_text number_with_delimiter(racial_diversity_grouped_by_region["Norte"]["nao_declarado"])
     end
-
     within(id: "branca-regiao-norte") do
-      assert_text number_with_delimiter(racial_diversity_by_region["branca"])
+      assert_text number_with_delimiter(racial_diversity_grouped_by_region["Norte"]["branca"])
     end
-
     within(id: "parda-regiao-norte") do
-      assert_text number_with_delimiter(racial_diversity_by_region["parda"])
+      assert_text number_with_delimiter(racial_diversity_grouped_by_region["Norte"]["parda"])
     end
-
     within(id: "preta-regiao-norte") do
-      assert_text number_with_delimiter(racial_diversity_by_region["preta"])
+      assert_text number_with_delimiter(racial_diversity_grouped_by_region["Norte"]["preta"])
     end
-
     within(id: "amarela-regiao-norte") do
-      assert_text number_with_delimiter(racial_diversity_by_region["amarela"])
+      assert_text number_with_delimiter(racial_diversity_grouped_by_region["Norte"]["amarela"])
+    end
+    within(id: "indigena-regiao-norte") do
+      assert_text number_with_delimiter(racial_diversity_grouped_by_region["Norte"]["indigena"])
     end
 
-    within(id: "indigena-regiao-norte") do
-      assert_text number_with_delimiter(racial_diversity_by_region["indigena"])
-    end
+
   end
 end
