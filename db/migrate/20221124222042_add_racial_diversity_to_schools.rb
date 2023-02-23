54f30da0835e3a519d4c5d682f2f76d5dc24e0cf
Guilherme Rojas <guilhermehrojas@gmail.com>
2023-02-23 17:42:11 -0300

    Changes migration in order to fit in mysql dsl.

--- db/migrate/20221124222042_add_racial_diversity_to_schools.rb
+++ db/migrate/20221124222042_add_racial_diversity_to_schools.rb
@@ -1,5 +1,7 @@
 class AddRacialDiversityToSchools < ActiveRecord::Migration[7.0]
   def change
-    add_column :schools, :racial_diversity, :json
+    change_table(:schools) do |t|
+      t.column :racial_diversity, :json
+    end
   end
 end
