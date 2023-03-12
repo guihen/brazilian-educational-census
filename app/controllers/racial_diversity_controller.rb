69b9fd886e713e6e326d1c2ef6d300dbd3edc6c2
Guilherme Rojas <guilhermehrojas@gmail.com>
2023-03-12 19:21:38 -0300

    Changes the racial diversity homepage to show both 2021 and 2022 census
    NEXT STEPS:
    - Improve the presentation of the information
    - Develop another views grouping by racial diversity, so we can see if the number of student are growing or shrinking by year
    - It would be nice to see statistics about this data

--- app/controllers/racial_diversity_controller.rb
+++ app/controllers/racial_diversity_controller.rb
@@ -1,15 +1,33 @@
 class RacialDiversityController < ApplicationController
   def index
-    @racial_diversity = Hash.new
-    @racial_diversity["Total"] = sum_racial_diversity(School.pluck(:racial_diversity))
-    @racial_diversity["Norte"] = sum_racial_diversity(School.where(region: "Norte").pluck(:racial_diversity))
-    @racial_diversity["Nordeste"] = sum_racial_diversity(School.where(region: "Nordeste").pluck(:racial_diversity))
-    @racial_diversity["Centro-Oeste"] = sum_racial_diversity(School.where(region: "Centro-Oeste").pluck(:racial_diversity))
-    @racial_diversity["Sudeste"] = sum_racial_diversity(School.where(region: "Sudeste").pluck(:racial_diversity))
-    @racial_diversity["Sul"] = sum_racial_diversity(School.where(region: "Sul").pluck(:racial_diversity))
+    @racial_diversity_2021 = Hash.new.tap do |hash|
+      hash["Total"] = sum_racial_diversity(scope_for_2021.pluck(:racial_diversity))
+      hash["Norte"] = sum_racial_diversity(scope_for_2021.where(region: "Norte").pluck(:racial_diversity))
+      hash["Nordeste"] = sum_racial_diversity(scope_for_2021.where(region: "Nordeste").pluck(:racial_diversity))
+      hash["Centro-Oeste"] = sum_racial_diversity(scope_for_2021.where(region: "Centro-Oeste").pluck(:racial_diversity))
+      hash["Sudeste"] = sum_racial_diversity(scope_for_2021.where(region: "Sudeste").pluck(:racial_diversity))
+      hash["Sul"] = sum_racial_diversity(scope_for_2021.where(region: "Sul").pluck(:racial_diversity))
+    end
+
+    @racial_diversity_2022 = Hash.new.tap do |hash|
+      hash["Total"] = sum_racial_diversity(scope_for_2022.pluck(:racial_diversity))
+      hash["Norte"] = sum_racial_diversity(scope_for_2022.where(region: "Norte").pluck(:racial_diversity))
+      hash["Nordeste"] = sum_racial_diversity(scope_for_2022.where(region: "Nordeste").pluck(:racial_diversity))
+      hash["Centro-Oeste"] = sum_racial_diversity(scope_for_2022.where(region: "Centro-Oeste").pluck(:racial_diversity))
+      hash["Sudeste"] = sum_racial_diversity(scope_for_2022.where(region: "Sudeste").pluck(:racial_diversity))
+      hash["Sul"] = sum_racial_diversity(scope_for_2022.where(region: "Sul").pluck(:racial_diversity))
+    end
   end
 
   private
+
+  def scope_for_2021
+    School.where(census_year: 2021)
+  end
+
+  def scope_for_2022
+    School.where(census_year: 2022)
+  end
 
   def sum_racial_diversity(scope)
     SumRacialDiversity.new(scope).call
