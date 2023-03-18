401369dbefcd9d5dde841fd14a74defb2c1d46ea
Guilherme Rojas <guilhermehrojas@gmail.com>
2023-03-18 17:20:22 -0300

    Removes 2021 data by region in homepage
    DECISIONS:
    - easiest way to make the page loads faster;
    - is this comparison a good data? If so, I can do it better latter.

--- app/controllers/racial_diversity_controller.rb
+++ app/controllers/racial_diversity_controller.rb
@@ -1,13 +1,8 @@
 class RacialDiversityController < ApplicationController
   def index
-    @racial_diversity_2021 = Hash.new.tap do |hash|
-      hash["Total"] = CalculatedRacialDiversity.find_by_slug('totals_2021').value
-      hash["Norte"] = sum_racial_diversity(scope_for_2021.where(region: "Norte").pluck(:racial_diversity))
-      hash["Nordeste"] = sum_racial_diversity(scope_for_2021.where(region: "Nordeste").pluck(:racial_diversity))
-      hash["Centro-Oeste"] = sum_racial_diversity(scope_for_2021.where(region: "Centro-Oeste").pluck(:racial_diversity))
-      hash["Sudeste"] = sum_racial_diversity(scope_for_2021.where(region: "Sudeste").pluck(:racial_diversity))
-      hash["Sul"] = sum_racial_diversity(scope_for_2021.where(region: "Sul").pluck(:racial_diversity))
-    end
+    @racial_diversity_2021 = {
+      "Total" => CalculatedRacialDiversity.find_by_slug('totals_2021').value
+    }
 
     @racial_diversity_2022 = Hash.new.tap do |hash|
       hash["Total"] = CalculatedRacialDiversity.find_by_slug('totals_2022').value
